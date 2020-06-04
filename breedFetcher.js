67a9428b40d272fafe33a0205475f2f5dfd253b8
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-04 23:19:41 +0000

    Updating and refactoring the cat breed function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,41 +1,17 @@
 const request = require("request"); 
-const fs = require('fs');
 
-let userInput = process.argv.slice(2);
+let breedName = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log(`Error text: ${error}` )
-  };
-  if(data[0] === undefined) {
-    console.log("Sorry, that search didn't turn anything up.")
-  } else {
-    console.log(data[0]);
-  };
-});
-
-
-
-//  console.log(`Data type is: ${typeof data}`);
-
-//OLD FETCHER FUNCTION FROM W2D3
-//console.log(requestArgs);
-
-/* request(requestArgs[0], (error, response, body) => {
-  if(error) {
-    console.log("There appears to be something wrong with the URL...")
-  }
-  fs.writeFile(requestArgs[1], body, (error) => {
-    if(error) {
-      console.log("Oops. Try again.");
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, resp, body) => {
+    if (error) {
+      return callback(error, null);
     }
-    if(fs.exists(`./${requestArgs[1]}`)) {
-      console.log("File already exists. Please choose another.")
-      return
+    let data = JSON.parse(body);
+    if (data[0]) {
+      return callback(null, data[0]);
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${requestArgs[1]}`)
-  });  
-});
-console.log('hello');
- */
+  });
+}
+
+module.exports = { fetchBreedDescription };
